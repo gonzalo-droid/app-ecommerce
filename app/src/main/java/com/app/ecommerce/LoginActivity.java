3c14213e33951d87d49df283c317c7f3c697993e
Walter Ocampo <fearless347@gmail.com>
2020-07-04 18:06:42 -0500

    Diseño Login y Registro V1

---
+++ app/src/main/java/com/app/ecommerce/LoginActivity.java
@@ -0,0 +1,39 @@
+package com.app.ecommerce;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    ImageView btnBack;
+    TextView btnRegister;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        btnBack = findViewById(R.id.buttonBack);
+        btnRegister = findViewById(R.id.buttonRegister);
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent test = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(test);
+            }
+        });
+
+    }
+}
