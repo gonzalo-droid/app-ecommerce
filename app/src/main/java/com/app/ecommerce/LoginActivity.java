2cfeed2fa00bef49b1e93db36fdf0fbfd5c6bf01
Walter Ocampo <fearless347@gmail.com>
2020-07-05 15:32:51 -0500

    Readability for the Project

--- app/src/main/java/com/app/ecommerce/LoginActivity.java
+++ app/src/main/java/com/app/ecommerce/LoginActivity.java
@@ -4,23 +4,18 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
-import com.facebook.appevents.AppEventsLogger;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
-import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
@@ -28,8 +23,6 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-
-import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class LoginActivity extends AppCompatActivity {
@@ -44,16 +37,17 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        // Botones en LoginActivity
+        // Buttons
         btnRegister = findViewById(R.id.buttonRegister);
         btnLogin = findViewById(R.id.buttonLogin);
         btnFb = findViewById(R.id.buttonFb);
 
+        // Instances of Facebook and Firebase
         FacebookSdk.sdkInitialize(LoginActivity.this);
         mAuth = FirebaseAuth.getInstance();
         mCallbackManager = CallbackManager.Factory.create();
 
-        // Clicks en botones
+        // Clicks
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -68,7 +62,7 @@ public class LoginActivity extends AppCompatActivity {
                 LoginManager.getInstance().registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
                     @Override
                     public void onSuccess(LoginResult loginResult) {
-                        // Log.d(TAG, "facebook:onSuccess:" + loginResult);
+                        // Get Token from Facebook
                         handleFacebookAccessToken(loginResult.getAccessToken());
                     }
 
@@ -95,26 +89,23 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     public void onStart() {
         super.onStart();
-        // Check if user is signed in (non-null) and update UI accordingly.
+        // Verify the session with Facebook
         FirebaseUser currentUser = mAuth.getCurrentUser();
         if (currentUser != null) updateUI(currentUser);
     }
 
     private void handleFacebookAccessToken(AccessToken token) {
-        // Log.d(TAG, "handleFacebookAccessToken:" + token);
         AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        // login with Firebase with Token
         mAuth.signInWithCredential(credential)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            // Log.d(TAG, "signInWithCredential:success");
+                            // Get user logged in
                             FirebaseUser user = mAuth.getCurrentUser();
                             updateUI(user);
                         } else {
-                            // If sign in fails, display a message to the user.
-                            // Log.w(TAG, "signInWithCredential:failure", task.getException());
                             Toast.makeText(LoginActivity.this, "Authentication failed.",
                                     Toast.LENGTH_SHORT).show();
                         }
@@ -128,21 +119,23 @@ public class LoginActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
     }
 
+    // Get access to Menu Activity
     private void updateUI(FirebaseUser user) {
         if (user != null) goMenu();
         else Toast.makeText(this, "This user doesn't exists", Toast.LENGTH_SHORT).show();
     }
 
+    // Go to Register Activity
     private void goRegisterActivity() {
-        Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+        Intent intent = new Intent(this, RegisterActivity.class);
         startActivity(intent);
     }
 
+    // Go to Menu Activity
     private void goMenu(){
         // getCarga();
         Intent i = new Intent(this,MenuActivity.class);
         i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(i);
-       // finish();
     }
 }
