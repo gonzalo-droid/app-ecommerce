71b94efa75c0306874a51beb18d88a1e253ea45f
ghost2050 <gonzalolopezguerrero2050@gmail.com>
2020-07-05 02:51:43 -0500

    add component navigation

--- app/src/main/java/com/app/ecommerce/LoginActivity.java
+++ app/src/main/java/com/app/ecommerce/LoginActivity.java
@@ -3,7 +3,9 @@ package com.app.ecommerce;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -11,6 +13,7 @@ public class LoginActivity extends AppCompatActivity {
 
     ImageView btnBack;
     TextView btnRegister;
+    Button buttonLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +22,14 @@ public class LoginActivity extends AppCompatActivity {
 
         btnBack = findViewById(R.id.buttonBack);
         btnRegister = findViewById(R.id.buttonRegister);
+        buttonLogin = findViewById(R.id.buttonLogin);
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goMenu();
+            }
+        });
 
         btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,5 +46,16 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+
+
+    }
+
+
+    private void goMenu(){
+        // getCarga();
+        Intent i = new Intent(this,MenuActivity.class);
+        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(i);
+       // finish();
     }
 }
